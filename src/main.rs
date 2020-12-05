adc7b7e2342cc3804b429473541edfc4ab81b318
eikendev <raphael@eiken.dev>
2020-12-05 16:57:14 +0100

    Add option to read from file

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
 use clap::{crate_authors, crate_description, crate_name, crate_version, load_yaml, value_t_or_exit};
-use futures::channel::mpsc::channel;
+use futures::channel::mpsc::{channel, Sender};
 use futures::{Stream, StreamExt};
 use http::StatusCode;
 use reqwest::Client;
+use std::fs::File;
 use std::io::BufRead;
 use std::process::exit;
 use std::time::Duration;
 
-fn stdin(threads: usize) -> impl Stream<Item = String> {
-    let (mut tx, rx) = channel(threads);
+fn push_lines(reader: &mut dyn BufRead, sendto: &mut Sender<String>) {
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            loop {
+                let status = sendto.try_send(line.to_owned());
 
-    std::thread::spawn(move || {
-        for line in std::io::stdin().lock().lines() {
-            if let Ok(line) = line {
-                loop {
-                    let status = tx.try_send(line.to_owned());
-
-                    match status {
-                        Err(e) if e.is_full() => continue,
-                        _ => break,
-                    }
+                match status {
+                    Err(e) if e.is_full() => continue,
+                    _ => break,
                 }
             }
+        }
+    }
+}
+
+fn get_lines(f: Option<File>, threads: usize) -> impl Stream<Item = String> {
+    let (mut tx, rx) = channel(threads);
+
+    std::thread::spawn(move || match f {
+        None => {
+            let stdin = std::io::stdin();
+            let mut reader = &mut stdin.lock() as &mut dyn BufRead;
+            push_lines(&mut reader, &mut tx);
+        }
+        Some(f) => {
+            let mut reader = &mut std::io::BufReader::new(f) as &mut dyn BufRead;
+            push_lines(&mut reader, &mut tx);
         }
     });
 
@@ -53,14 +66,14 @@ async fn process_url(url: String, client: &Client, quiet: bool) -> bool {
 }
 
 #[tokio::main]
-async fn process_urls(quiet: bool, threads: usize, timeout: u64, useragent: &str) -> bool {
+async fn process_urls(quiet: bool, threads: usize, timeout: u64, useragent: &str, file: Option<File>) -> bool {
     let client = reqwest::Client::builder()
         .timeout(Duration::from_secs(timeout))
         .user_agent(useragent)
         .build()
         .unwrap();
 
-    let statuses = stdin(threads)
+    let statuses = get_lines(file, threads)
         .map(|url| {
             let client = &client;
 
@@ -88,7 +101,19 @@ fn main() {
     let timeout = value_t_or_exit!(matches.value_of("timeout"), u64);
     let useragent = value_t_or_exit!(matches.value_of("useragent"), String);
 
-    let success = process_urls(quiet, threads, timeout, &useragent);
+    let file = value_t_or_exit!(matches.value_of("FILE"), String);
+    let file: Option<File> = match file {
+        _ if file == "-" => None,
+        _ => match File::open(file) {
+            Ok(f) => Some(f),
+            Err(e) => {
+                eprintln!("Cannot read specified file: {}", e);
+                exit(1)
+            }
+        },
+    };
+
+    let success = process_urls(quiet, threads, timeout, &useragent, file);
 
     if !success {
         exit(1);
